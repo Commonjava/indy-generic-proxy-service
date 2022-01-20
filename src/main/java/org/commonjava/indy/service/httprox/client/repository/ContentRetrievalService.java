c12728cbbcc647005978bbb28a03f1a433c1114e
Wenjie Guo <wguo@redhat.com>
2022-01-20 15:13:46 +0800

    Init content retrieval service client

---
+++ src/main/java/org/commonjava/indy/service/httprox/client/repository/ContentRetrievalService.java
@@ -0,0 +1,19 @@
+package org.commonjava.indy.service.httprox.client.repository;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/api/content/generic-http")
+@RegisterRestClient(configKey="content-retrieval-service-api")
+public interface ContentRetrievalService
+{
+
+    @GET
+    @Path("/{type: (hosted|group|remote)}/{name}/{path: (.+)}")
+    Response doGet(@PathParam( "type" ) String type, @PathParam( "name" ) String name, final @PathParam( "path" ) String path);
+
+}
